70f1b3bb3cb0c9ebe5699701fb478ea74f3a01b4
Johan Gorter <johan-gorter@users.noreply.github.com>
2024-10-03 08:45:40 +0200

    del ui script

---
+++ delete-ui.cmd
@@ -0,0 +1,6 @@
+del .\LearnFromAI.Web\Pages\Index.*
+del .\LearnFromAI.Web\Pages\Subject.*
+del .\LearnFromAI.Web\Pages\Exercise.*
+del .\LearnFromAI.Web\Pages\Shared\_Layout.*
+del .\LearnFromAI.Web\Pages\Shared\_TableOfContents.*
+del .\LearnFromAI.Web\Pages\Shared\_ValidationScriptsPartial.*
