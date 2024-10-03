f96a9c431ccdb43d1b65dc75ad84e71b8334a3c3
Johan Gorter <johan-gorter@users.noreply.github.com>
2024-10-03 08:35:34 +0200

    namespace fixes

--- LearnFromAI.Web/Pages/Login.cshtml.cs
+++ LearnFromAI.Web/Pages/Login.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 
-namespace LearnFromAI.Pages
+namespace LearnFromAI.Web.Pages
 {
   [AllowAnonymous]
   public class LoginModel : PageModel
--- LearnFromAI.Web/Pages/Privacy.cshtml.cs
+++ LearnFromAI.Web/Pages/Privacy.cshtml.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LearnFromAI.Pages;
+namespace LearnFromAI.Web.Pages;
 
 public class PrivacyModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
+  private readonly ILogger<PrivacyModel> _logger;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
-    {
-        _logger = logger;
-    }
+  public PrivacyModel(ILogger<PrivacyModel> logger)
+  {
+    _logger = logger;
+  }
 
-    public void OnGet()
-    {
-    }
+  public void OnGet()
+  {
+  }
 }
 
